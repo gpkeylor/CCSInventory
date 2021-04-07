b0e25d11bfff00494450fcdc245297d7ce4da306
Braden Carlson <bmcarlson@crimson.ua.edu>
2021-04-07 17:14:55 -0500

    mySqlConnectionString

---
+++ API/ConnectionString.cs
@@ -0,0 +1,18 @@
+namespace API
+{
+    public class ConnectionString
+    {
+        public string cs{get;  set;}
+
+        public ConnectionString()
+        {
+            string server = "jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+            string database = "zpsfskh0flhfzhe6";
+            string port= "3306";
+            string username = "m173a7fuq4qs77yi";
+            string password = "vegnyi6cl18z26ig";
+
+            cs = $@"server = {server};user={username};database={database};port={port};password={password};";
+        }
+    }
+}
