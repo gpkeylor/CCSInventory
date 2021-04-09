45cb59b7a762c205252e00470c4f546daa9edd80
Braden Carlson <bmcarlson@crimson.ua.edu>
2021-04-09 18:06:28 -0500

    Transaction Constructor Added

---
+++ API/Database/SaveTransaction.cs
@@ -0,0 +1,7 @@
+namespace API.Database
+{
+    public class SaveTransaction
+    {
+        
+    }
+}
--- API/Models/Transaction.cs
+++ API/Models/Transaction.cs
@@ -8,8 +8,15 @@ namespace API.Models
         public int ItemID{get;set;}
         public DateTime CheckOutDate{get; set;}
         public DateTime DueDate{get; set;}
-        public DateTime ReturnDate{get; set;}
+        public DateTime? ReturnDate{get; set;} //? allows ReturnDate to be null
         public int AdminID{get; set;}
+
+        public Transaction() //when a new transaction is created sets DueDate to two weeks from day of checkout
+        {
+            DueDate = DateTime.Now.AddDays(14);
+            CheckOutDate = DateTime.Now;
+            ReturnDate = null;
+        }
         
         
     }
