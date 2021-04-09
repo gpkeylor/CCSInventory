794c752e918c1ee7169af84be676fd8a62dc50f6
Braden Carlson <bmcarlson@crimson.ua.edu>
2021-04-08 21:42:36 -0500

    OverdueReport class revised

--- API/Models/OverdueReport.cs
+++ API/Models/OverdueReport.cs
@@ -2,6 +2,7 @@ namespace API.Models
 {
     public class OverdueReport
     {
+        public int EmpID{get; set;}
         public string EmpEmail{get; set;}
         public int DaysOverdue{get; set;}
     }
