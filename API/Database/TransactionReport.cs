c11bc2257cd0cd012c91228cd0747b9ccf4283bd
Braden Carlson <bmcarlson@crimson.ua.edu>
2021-04-08 21:30:39 -0500

    OverdueReport

---
+++ API/Database/TransactionReport.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using API.Models;
+using MySql.Data.MySqlClient;
+
+namespace API.Database
+{
+    public class TransactionReport
+    {
+        public List<OverdueReport> OverdueStatus()
+        {
+            ConnectionString myConnection = new ConnectionString();
+            string cs = myConnection.cs;
+            using var con = new MySqlConnection(cs);
+
+            con.Open();
+
+            string stm = "SELECT e.empemail, DATEDIFF(day, t.duedate, GetDate()) as daysoverdue FROM transaction t join employee e on (t.empid = e.empid) WHERE DATEDIFF(day, t.duedate, GetDate()) > 0";
+            using var cmd = new MySqlCommand(stm,con);
+
+            using MySqlDataReader rdr = cmd.ExecuteReader();
+
+            List<OverdueReport> overdueList = new List<OverdueReport>();
+            while(rdr.Read())
+            {
+                OverdueReport overdueReport = new OverdueReport(){EmpEmail = rdr.GetString(0), DaysOverdue = rdr.GetInt32(1)};
+                overdueList.Add(overdueReport);
+            }
+            return overdueList;
+        }
+    }
+}
---
+++ API/Models/OverdueReport.cs
@@ -0,0 +1,8 @@
+namespace API.Models
+{
+    public class OverdueReport
+    {
+        public string EmpEmail{get; set;}
+        public int DaysOverdue{get; set;}
+    }
+}
