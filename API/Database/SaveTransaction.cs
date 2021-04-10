89ebe2d36f080e08fcfcf296d779852443f2c847
Braden Carlson <bmcarlson@crimson.ua.edu>
2021-04-10 13:05:17 -0500

    SaveTransactionClass Created

--- API/Database/SaveTransaction.cs
+++ API/Database/SaveTransaction.cs
@@ -2,6 +2,54 @@ namespace API.Database
 {
     public class SaveTransaction
     {
-        
+        public void AddTransaction(Transaction transaction) //Checkout Function
+        {
+            ConnectionString myConnection = new ConnectionString();
+            string cs = myConnection.cs;
+            using var con = new MySqlConnection(cs);
+
+            con.Open();
+
+            string stm = "INSERT INTO transaction (empid, itemid, adminid) VALUES(@empid, @itemid, @adminid)"; //don't need to insert checkedoutdate, duedate, or returndate 
+            using var cmd = new MySqlCommand(stm,con);                                                         //because they are initialized in the constructor when a transaction is created
+            cmd.Parameters.AddWithValue("@empid", transaction.EmpID);
+            cmd.Parameters.AddWithValue("@transactioncomments", transaction.ItemID);
+            cmd.Parameters.AddWithValue("@transactioncheckedoutstatus", transaction.AdminID);
+            cmd.Prepare();
+            
+            cmd.ExecuteNonQuery();
+        }
+
+        public void UpdateTransactionReturnDate(Transaction transaction) //ReturnTransaction
+        {
+            ConnectionString myConnection = new ConnectionString();
+            string cs = myConnection.cs;
+            using var con = new MySqlConnection(cs);
+
+            con.Open();
+
+            string stm = "UPDATE Transaction SET returndate = @returndate WHERE transactionID = @transactionID";
+            using var cmd = new MySqlCommand(stm,con);
+            cmd.Parameters.AddWithValue("@transactionID", transaction.TransactionID);
+            cmd.Parameters.AddWithValue("@returndate", transaction.ReturnDate);
+            cmd.Prepare();
+            
+            cmd.ExecuteNonQuery();
+        }
+        public void DeleteTransaction(Transaction transaction)
+        {
+            ConnectionString myConnection = new ConnectionString();
+            string cs = myConnection.cs;
+            using var con = new MySqlConnection(cs);
+
+            con.Open();
+
+            string stm = "DELETE FROM Transaction WHERE transactionID = @transactionID";
+            using var cmd = new MySqlCommand(stm,con);
+            cmd.Parameters.AddWithValue("@transactionID", transaction.transactionID);
+            cmd.Prepare();
+            
+            cmd.ExecuteNonQuery();
+        }
     }
 }
