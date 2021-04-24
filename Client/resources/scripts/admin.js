8188d50d5dcbbcad86eca01f77525cdbd2d54f3d
Braden Carlson <bmcarlson@crimson.ua.edu>
2021-04-23 20:59:04 -0500

    Resolved Conflicts in branches

--- Client/resources/scripts/admin.js
+++ Client/resources/scripts/admin.js
@@ -12,17 +12,8 @@ function getAdministrator()
         json.forEach(administrator => {
             if (administrator.adminID == enteredId)
             {
-<<<<<<< HEAD
-              //  console.log(administrator.adminID);
-               // document.getElementById("administrator").setAttribute("href","./transactions.html");
-               window.location ="\ transaction.html";
-            }
-            else{
-                displayErrorMessage();
-=======
               
                window.location ="\ transaction.html";
->>>>>>> f186271d839f43da5d1ea2c2fa131c654d7bd247
             }
         });
        
